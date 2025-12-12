73d1e757082fd67b36f6cc8a19833370dccaae68
Herry <1944136933@qq.com>
2025-12-12 10:58:57 +0800

    chore: 修复 PolygonCanvas 渲染问题

--- src/components/polygon-editor/components/PolygonCanvas.tsx
+++ src/components/polygon-editor/components/PolygonCanvas.tsx
@@ -156,7 +156,7 @@ export function PolygonCanvas({
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
-          imageRendering: 'high-quality',
+          imageRendering: 'auto',
           ...getCanvasStyle(),
         }}
       >
